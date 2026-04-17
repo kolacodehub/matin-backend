7dd3a7fca250aa2c1be21ef36df043a4235bd7c8
Tiamiyu Abdul-salam <128032488+kolacodehub@users.noreply.github.com>
2026-04-17 05:16:45 +0100

    k

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import QFAuthExchangeView, LogoutView
 
 urlpatterns = [
-    path("auth/qf-exchange/", QFAuthExchangeView.as_view(), name="qf-exchange"),
-    path("logout/", LogoutView.as_view(), name="logout"), # <-- ADD THIS
+    path("qf-exchange/", QFAuthExchangeView.as_view(), name="qf-exchange"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
